44f9bf72a86bdcb7bf655dd0df37ff6bc5ce8321
arshavir mirzakhani <arshavir2468@gmail.com>
2024-09-03 16:19:30 +0330

    added node deleting option

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import PySide6.QtCore as QtCore
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import *
 from nodes.Nodes import *
 from Graph import Graph
 
@@ -9,25 +9,30 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Lambda2")
+        self.setWindowTitle('Lambda2')
 
         node_graph = Graph()
         node_graph.register_node(ConstantNode)
         node_graph.register_node(LabelNode)
         node_graph.register_node(AddNode)
 
-        context_menu = node_graph.get_context_menu("graph")
-        nodes_menu = context_menu.add_menu("Create Node")
+        graph_context_menu = node_graph.get_context_menu('graph')
+        graph_nodes_menu = graph_context_menu.add_menu('Create Node')
 
-        nodes_menu.add_command('Constant',lambda : node_graph.create_node("Nodes.ConstantNode"))
-        nodes_menu.add_command('Label',lambda : node_graph.create_node("Nodes.LabelNode"))
-        nodes_menu.add_command('Add',lambda : node_graph.create_node("Nodes.AddNode"))
+        graph_nodes_menu.add_command('Constant',lambda : node_graph.create_node('Nodes.ConstantNode'))
+        graph_nodes_menu.add_command('Label',lambda : node_graph.create_node('Nodes.LabelNode'))
+        graph_nodes_menu.add_command('Add',lambda : node_graph.create_node('Nodes.AddNode'))
 
-        node_graph_widget = node_graph.widget
-        self.setCentralWidget(node_graph_widget)
+        
+        node_context_menu = node_graph.get_context_menu('nodes')
+
+        node_context_menu.add_command('Delete node',lambda graph,node : graph.remove_node(node),'Del',node_class=BaseNode)
 
 
-if __name__ == "__main__":
+        self.setCentralWidget(node_graph.widget)
+
+
+if __name__ == '__main__':
     app = QApplication(sys.argv)
 
     window = MainWindow()
